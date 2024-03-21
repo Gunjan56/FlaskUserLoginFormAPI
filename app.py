e9bbc7d07d810abb91a1ccbf1d9a134fcba2e9db
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-03-21 19:36:23 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from models.model import db, User
+import re
 
 
 app = Flask(__name__)
@@ -8,21 +9,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'gunjan'
 db.init_app(app) 
 migrate = Migrate(app, db)
+def validation(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if re.fullmatch(regex, email):
+        return "valid email"
+    else:
+        "Invalid Email"
 
 
-
-@app.route('/', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
     data = request.get_json();
     username = data['username']
     email = data['email']
     password = data['password']
+    userDetails = User.query.filter_by(username=username, email=email, password=password).first()
 
+    if userDetails:
+        return jsonify({"message": "User already registered"} )
     user = User(username=username, email=email, password=password)
     db.session.add(user)
     db.session.commit()
-
-    return jsonify({'message': 'User Registered successfully'})
+    return jsonify({'message': 'User registered successfully'})
 
 
 def login():
@@ -31,7 +39,7 @@ def login():
     email = data['email']
     password = data['password']
 
-    user = User.query.filter_by(username=username, email=email, password=password)
+    user = User.query.filter_by(username=username, email=email, password=password).first()
 
     if user:
         return jsonify({'message': "You login successfully!"})
