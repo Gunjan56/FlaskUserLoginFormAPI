1ef7a078f7809019153ca95dbf1fd12e42295c2d
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-03-22 11:50:18 +0530

    Code for Review

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from models.model import db, User
+from werkzeug.security import check_password_hash, generate_password_hash
 import re
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'gunjan'
 db.init_app(app) 
 migrate = Migrate(app, db)
 
+email_Validation = r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -17,14 +17,24 @@ def register():
     username = data['username']
     email = data['email']
     password = data['password']
-    userDetails = User.query.filter_by(username=username, email=email, password=password).first()
+    pass_validate = generate_password_hash(password)
 
+    if not re.match(email_Validation, email):
+        return jsonify({'message': 'Enter a valid email'}), 400
+    
+    userDetails = User.query.filter_by(username=username, email=email, password=password).first()
+    
+    
+    
     if userDetails:
         return jsonify({"message": "User already registered"} )
-    user = User(username=username, email=email, password=password)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({'message': 'User registered successfully'})
+    
+    else:
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({'message': 'User registered successfully'})
 
 
 def login():
@@ -35,12 +45,11 @@ def login():
 
     user = User.query.filter_by(username=username, email=email, password=password).first()
 
-    if user:
+    if user and check_password_hash(user.password, password):
         return jsonify({'message': "You login successfully!"})
     else:
         return jsonify({'message': 'Enter valid credentials to login!'})
-
-
+    
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
--- models/model.py
+++ models/model.py
@@ -4,6 +4,6 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False, unique=True)
     
