5da66a4fe375c65e37663bcd91e9f5543b54a042
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-03-23 19:34:15 +0530

    Code for review

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def login():
     password = data['password']
    
 
-    user = User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(email=email).first()
 
     if user and check_password_hash(user.password, password):
         access_token = create_access_token(identity=email)
